f251f27a91a6a3c4f2c3a93a674a21ce20f38f00
Francisco Pacio <fpaciook@gmail.com>
2020-09-14 23:38:42 -0300

    Generic convolution & Edge detection

--- Filtros de imagenes/entregable/src/filters.cpp
+++ Filtros de imagenes/entregable/src/filters.cpp
@@ -22,7 +22,6 @@ void blackWhite(ppm& img)
 	for(y = 0; y < img.height; y++){
 
 		for(x = 0; x < img.width; x++){
-			
 			// Process every pixel values
 			r = img.getPixel(y,x).r;
 			g = img.getPixel(y,x).g;
@@ -48,7 +47,6 @@ void merge(ppm& img1, ppm& img2, float alpha)
 	for(y = 0; y < img.height; y++){
 		
 		for(x = 0; x < img.width; x++){
-			
 			// Process every pixel values
 			r = (img1.getPixel(y,x).r * alpha) + ( img2.getPixel(y,x).r * (1- alpha));
 			g = (img1.getPixel(y,x).g * alpha) + ( img2.getPixel(y,x).g * (1- alpha));
@@ -71,7 +69,6 @@ void brightness(ppm& img, float b, int start, int end)
 	for(y = 0; y < img.height; y++){
 		
 		for(x = 0; x < img.width; x++){
-			
 			// Process every pixel values
 			r = img.getPixel(y,x).r + ( 255 * b );
 			g = img.getPixel(y,x).g + ( 255 * b ):
@@ -94,10 +91,10 @@ void contrast(ppm& img, float contrast)
 	f = 259*(contrast+255) / 255*(259-contrast);
 
 	// Read the image
+
 	for(y = 0; y < img.height; y++){
 
 		for(x = 0; x < img.width; x++){
-
 			// Process every pixel values
 			r = f * (img.getPixel(y,x).r - 128) + 128;
 			g = f * (img.getPixel(y,x).g - 128) + 128;
@@ -107,6 +104,46 @@ void contrast(ppm& img, float contrast)
 			img.setPixel(y,x,pixel(r,g,b));
 		}
 	}
+
+	return;
+}
+
+ppm* convolution(ppm& img, short int ker[])
+{
+	// Declare the needed variables
+	ppm* newImg(img.height-2, img.width-2);
+	short int r,g,b=0;
+
+	// Read the image
+	for(int y = 1; y < img.height - 1; y++){
+
+		for(int x = 1; x < img.width - 1; x++){
+			r,g,b=0;
+
+			// Run the kernel over the image
+			for(int ky = 0; ky < 4; ky++){
+
+				for(int kx = 0; kx < 4; kx++){
+					r += img.getPixel(y+ky-1,x+kx-1).r * ker[ky*3+kx];
+					g += img.getPixel(y+ky-1,x+kx-1).g * ker[ky*3+kx];
+					b += img.getPixel(y+ky-1,x+kx-1).b * ker[ky*3+kx];
+				}
+			}
+
+			// Set the result
+			newImg.setPixel(y,x,pixel(r,g,b));
+		}
+	}
+
+	return newImg;
+}
+
+void edgeDetection(ppm &img, ppm &img_target){
+	// Declare the kernel
+	int kernel[] = {1, 0, -1, 2, 0, -2, 1, 0, -1};
+
+	// Set the result
+	img_target = convolution(img, ker);
 
 	return;
 }
