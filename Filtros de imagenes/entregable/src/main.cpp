8de5e86a1cfc15e22dfca6135b9a668488326e88
Francisco Pacio <fpaciook@gmail.com>
2020-09-16 23:11:59 -0300

    Broken parser

--- Filtros de imagenes/entregable/src/main.cpp
+++ Filtros de imagenes/entregable/src/main.cpp
@@ -18,17 +18,56 @@ int main(int argc , char* argv[]){
 		cout << "Uso: ./tp <filtro> <nthreads> <p1> <p2> <img1> <img2>" << endl;
 		return 0; 
 	}
-	string img1(argv[1]);
-	ppm img(img1);
 
 	cout << "Aplicando filtro" << endl;
-
+	// Start the clock
 	struct timespec start, stop;
     double accum;
 	clock_gettime(CLOCK_REALTIME, &start);
 	
-	frame(img, pixel(100,100,100), 30);
-	
+	if(string(argv[1])=="blackWhite")
+	{
+
+		string img1(argv[3]);
+		ppm img(img1);
+		blackWhite(img);
+
+	}else if(string(argv[1])=="frame"){	
+
+		int pix(argv[3]);
+		int val(argv[4]);
+		string img1(argv[5]);
+		ppm img(img1);
+		frame(img,pixel(pix, pix, pix), val);
+
+	}else if(string(argv[1])=="brightness"){
+
+		float br(argv[3]);
+		brightness(img,br,0,0);
+
+	}else if(string(argv[1])=="merge"){
+
+		string img1(argv[4]);
+		ppm img(img1);
+		string imgb(argv[5]);
+		ppm img2(imgb);
+		float p1=(float)(argv[3]);
+		merge(img, img2, p1);
+
+	}else if(string(argv[1])=="edgeDetection"){
+
+		string img1(argv[3]);
+		ppm img(img1);
+		ppm img_target(img.width-2, img.height-2);
+		edgeDetection(img,img_target);
+	}else{
+		cout << "Error";
+		return 1;
+	}
+	string img1(argv[3]);
+	ppm img(img1);
+
+	// Stop the clock
 	clock_gettime(CLOCK_REALTIME, &stop);
 	accum = ( stop.tv_sec - start.tv_sec )
 	        + ( stop.tv_nsec - start.tv_nsec ) * ONE_OVER_BILLION;
@@ -36,6 +75,7 @@ int main(int argc , char* argv[]){
 	
 	cout << "Escribiendo imagen" << endl;
 
+	// Write the image
 	string out = "out/salida.ppm";
 	img.write(out); 
 
